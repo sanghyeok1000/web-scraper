fa2edc0d490203027efeac27991628c591d3e63d
sanghyeok1000 <parksang10000@naver.com>
2022-12-14 17:17:48 +0900

    saving result

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    result = []
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.find_all("section", class_="jobs"))
+    jobs = soup.find_all("section", class_="jobs")
     for job_section in jobs:
         job_posts = job_section.find_all("li")
         job_posts.pop(-1)
-        for post in job_post:
-            print(post)
+        for post in job_posts:
+            anchors = post.find_all("a")
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all("span", class_="company")
+            title = anchor.find("span", class_="title")
+            job_data = {
+                'company': company.string,
+                'region': region.string,
+                'position': title.string
+            }
+            result.append(job_data)
