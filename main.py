2b3fabb0115393886a0ea743932155386c3b4ab7
sanghyeok1000 <parksang10000@naver.com>
2022-12-14 16:20:07 +0900

    job_posts

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ if response.status_code != 200:
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     print(soup.find_all("section", class_="jobs"))
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop(-1)
+        for post in job_post:
+            print(post)
