cf08650687bdff9b9ab2018c961d17ef4b77950f
sanghyeok1000 <parksang10000@naver.com>
2022-12-14 19:55:59 +0900

    indeed site

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
 
-jobs = extract_wwr_jobs('python')
-print(jobs)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all("li", recursive=False)
