dca38d9d9a5ae6d2780b7698c7d03dbcf1349329
sanghyeok1000 <parksang10000@naver.com>
2022-12-15 15:02:58 +0900

    selenium installed

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disabled-dev-shm-usage")
+
+browser = webdriver.Chrome(options=options)
+
+browser.get("https://kr.indeed.com/jobs?q=python&limit=50")
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+
+
+"""
 base_url = "https://kr.indeed.com/jobs?q="
 search_term = "python"
 
@@ -12,3 +25,4 @@ else:
     soup = BeautifulSoup(response.text, "html.parser")
     job_list = soup.find("ul", class_="jobsearch-ResultsList")
     jobs = job_list.find_all("li", recursive=False)
+"""
